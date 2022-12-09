ba6c2e114015f2bc605e0825d0f13a4dd86b8eca
ironifster <ifeanyionyeanakwe@gmail.com>
2022-12-09 18:44:10 -0500

    Updating Angular, not main work

--- OpenSource/src/app/app.module.ts
+++ OpenSource/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SurveyCardComponent } from './components/survey-card/survey-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardComponent } from './components/mat-card/mat-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyCardComponent,
+    MatCardComponent
   ],
   imports: [
     BrowserModule,
