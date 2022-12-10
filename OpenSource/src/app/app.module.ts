1d712508cf19f380e2a049413809434acec72b0c
ironifster <ifeanyionyeanakwe@gmail.com>
2022-12-09 19:35:40 -0500

    Attempting update

--- OpenSource/src/app/app.module.ts
+++ OpenSource/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SurveyCardComponent } from './components/survey-card/survey-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
