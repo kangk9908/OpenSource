fda781e0070633d93fc11ad2731fd8f0992f8eb2
Ifeanyichukwu Onyeanakwe <ifeanyionyeanakwe@gmail.com>
2022-12-10 12:27:48 -0500

    Project Component and some work on the Devs page

--- OpenSource/src/app/app.module.ts
+++ OpenSource/src/app/app.module.ts
@@ -9,9 +9,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ProjectListComponent } from 'src/project/project_list'
 
 @NgModule({
-  declarations: [AppComponent, routingComponents],
+  declarations: [AppComponent, ProjectListComponent, routingComponents],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,8 +24,12 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatFormFieldModule,
     MatIconModule,
+    MatDialogModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+}
---
+++ OpenSource/src/project/project_list.ts
@@ -0,0 +1,26 @@
+/**
+ * @title Basic expansion panel
+ */
+
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-project_list',
+    templateUrl: './project_list.html',
+    styleUrls: ['./project_list.scss'],
+})
+
+export class ProjectListComponent implements OnInit {
+    @Input() projTitle?: string;
+    @Input() projDesc?: string;
+    @Input() imgs?: string;
+    panelOpenState = false;
+
+    constructor() {
+        this.projTitle = "How old is your dad?";
+        this.projDesc = "Really fun Prof. Norman Project";
+        this.imgs = "";
+    }
+
+    ngOnInit(): void { }
+}
