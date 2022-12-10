284880cbc6cbaca2758653f6529eebd5b872b011
Kun Kang <kangk9908@gmail.com>
2022-12-10 10:55:11 -0500

    fixed merge errors

---
+++ OpenSource/src/devs-page/devs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-devs',
+  templateUrl: './devs.component.html',
+  styleUrls: ['./devs.component.scss']
+})
+export class DevsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
