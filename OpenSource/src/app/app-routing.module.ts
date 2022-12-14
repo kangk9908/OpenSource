19e1bb24af0fb993becf511c03c61c65f790d41e
Kun Kang <kangk9908@gmail.com>
2022-12-14 00:58:57 -0500

    user info displayed, cannot go from login to dashboard

--- OpenSource/src/app/app-routing.module.ts
+++ OpenSource/src/app/app-routing.module.ts
@@ -2,24 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // pages
-import { LoginComponent } from 'src/account/sign-in/login';
+import { SignInComponent } from 'src/account/sign-in/sign-in';
 import { PageNotFoundComponent } from 'src/account/page-not-found/page_not_found';
-import { HomeComponent } from 'src/account/home/home.component';
+import { DashboardComponent } from 'src/account/dashboard/dashboard.component';
 import { DevsComponent } from 'src/devs-page/devs.component';
 import { SignUpComponent } from 'src/account/sign-up/sign-up';
 import { RecoverAccountComponent } from 'src/account/recover-account/recover_account';
 import { VerifyEmailComponent } from 'src/account/verify-email/verify-email.component';
 import { CommunityPostsComponent } from './community-posts/community-posts.component';
 
+// route guard
+import { AuthGuard } from './shared/guard/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: LoginComponent },
-  { path: 'dashboard', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'devs', component: DevsComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'recover-account', component: RecoverAccountComponent },
   { path: 'community-posts', component: CommunityPostsComponent },
-  { path: 'verify-email-address', component: VerifyEmailComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
   { path: '**', component: PageNotFoundComponent }, // must be at the end of the array
 ];
 
@@ -29,9 +32,9 @@ const routes: Routes = [
 })
 export class AppRoutingModule {}
 export const routingComponents = [
-  LoginComponent,
+  SignInComponent,
   PageNotFoundComponent,
-  HomeComponent,
+  DashboardComponent,
   DevsComponent,
   SignUpComponent,
   RecoverAccountComponent,
