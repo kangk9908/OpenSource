c0b3d929fc48c3f6e7df3b9e180a54fb617e771a
Sean Ebenmelu <jindusean@gmail.com>
2022-12-15 04:20:29 -0500

    ss

--- OpenSource/src/app/app.module.ts
+++ OpenSource/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 
 // Auth service
 import { AuthService } from './shared/services/auth';
