c4d440aa5fc08f1f300d666c882accfb059ed89c
Kun Kang <kangk9908@gmail.com>
2022-12-15 03:07:08 -0500

    displaying from database

--- OpenSource/src/account/dashboard/dashboard.component.ts
+++ OpenSource/src/account/dashboard/dashboard.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core'
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 import { AuthService } from 'src/app/shared/services/auth'
+
+interface Item {
+  projName: string
+  projDesc: string
+  progLang: string
+  specInstr: string
+  timestamp: firebase.default.firestore.Timestamp
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +16,13 @@ import { AuthService } from 'src/app/shared/services/auth'
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  projTitle = 'Whatever you want baby!'
-  projDesc = 'Really fun Prof. Norman Project'
-  imgs = ''
-  constructor(public authService: AuthService) {}
+  projLog: Array<Item> = []
+  constructor(public authService: AuthService, private db: AngularFirestore) {
+    db.collection<Item>('/project', (ref) => ref.orderBy('timestamp'))
+      .valueChanges()
+      .subscribe((res) => {
+        this.projLog = res
+      })
+  }
   ngOnInit() {}
 }
--- OpenSource/src/app/components/project/project_list.ts
+++ OpenSource/src/app/components/project/project_list.ts
@@ -3,6 +3,8 @@
  */
 
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core'
+import { AngularFirestore } from '@angular/fire/compat/firestore'
+import { Timestamp } from 'rxjs'
 
 @Component({
   selector: 'app-project_list',
@@ -10,16 +12,49 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core'
   styleUrls: ['./project_list.scss'],
 })
 export class ProjectListComponent implements OnInit {
-  @Input() projTitle?: string
+  @Input() projName?: string
   @Input() projDesc?: string
-  @Input() imgs?: string
+  @Input() progLang?: string
+  @Input() specInstr?: string
+
   panelOpenState = false
 
-  constructor() {
-    this.projTitle = 'How old is your dad?'
-    this.projDesc = 'Really fun Prof. Norman Project'
-    this.imgs = ''
+  constructor(private db: AngularFirestore) {}
+
+  scrollToBottom() {
+    window.scrollTo(0, document.body.scrollHeight)
   }
 
+  enterProjName(projName: string) {
+    this.projName = projName
+    localStorage.setItem('projName', projName)
+  }
+  enterProjDesc(projDesc: string) {
+    this.projDesc = projDesc
+    localStorage.setItem('projDesc', projDesc)
+  }
+  enterProgLang(progLang: string) {
+    this.progLang = progLang
+    localStorage.setItem('progLang', progLang)
+  }
+  enterSpecInstr(specInstr: string) {
+    this.specInstr = specInstr
+    localStorage.setItem('specInstr', specInstr)
+  }
+  makeProj() {
+    if (this.projName !== '') {
+      this.db.collection('project').add({
+        projName: localStorage.getItem('projName'),
+        projDesc: localStorage.getItem('projDesc'),
+        progLang: localStorage.getItem('progLang'),
+        specInstr: localStorage.getItem('specInstr'),
+        timestamp: new Date(),
+      })
+    }
+    this.projName = ''
+    this.projDesc = ''
+    this.progLang = ''
+    this.projName = ''
+  }
   ngOnInit(): void {}
 }
